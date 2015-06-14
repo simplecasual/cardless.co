316e68a0d6d5301f29073b422a583e4e4dd76889
Alex Baldwin <alexbaldwin@gmail.com>
2015-06-13 20:09:15 -0700

    Carrierwave and imgix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :image, ImageUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  mount_uploader :image, ImageUploader
 
   def to_param
     username
